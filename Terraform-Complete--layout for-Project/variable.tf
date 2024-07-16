7b9cbc8f3f4508d7e26e44b1f88848731565c560
saalim <ksaalim418@gmail.com>
2024-07-16 14:58:49 +0530

    variable.tf file

---
+++ Terraform-Complete--layout for-Project/variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+        description = "the region in which is created"
+        type = string
+        default = "ap-south-1"
+  
+}
+variable "key" {
+        description = "key for "
+        type = string
+        default = "ap-south-1"
+  
+}
